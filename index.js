894705c49fc677b2b1a05cba8db0f94d284412e2
alikiminza <ali.kiminza@student.moringaschool.com>
2023-06-19 18:04:57 +0300

    edited

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// code your solution here
+// // code your solution here
+// let activity;
+
+
+// function saturdayFun(activity = 'roller-skate'){
+//     return `This saturday, I want to ${activity}!`;
+// }
+// console.log ( saturdayFun(activity))
+//let bathe;
+//let skate;
+// function saturdayFun(skate) {
+//     return 'This Saturday, I want to roller-skate!'
+
+// }
+//console.log(saturdayFun(skate));
+function saturdayFun(bathe = 'roller-skate') {
+    return `This Saturday, I want to ${bathe}!`
+}
+//console.log(saturdayFun(bathe));
+let office;
+function mondayWork(office = 'go to the office') {
+    return `This Monday, I will ${office}.`
+}
+// function mondayWork(home) {
+//     return 'This Monday, I will work from home.'
+// }
+function wrapAdjective(asterix = "*") {
+    return function (adjective="special"){
+        return `You are ${asterix}${adjective}${asterix}!`;
+    }
+    console.log(wrapAdjective(asterix));
+    
+}
